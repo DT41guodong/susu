c569c0e660fdf5760d45dccb5baaf6f0f6386ba0
dt41shuying <13164666330@163.com>
2017-10-14 14:08:41 +0800

    修改了、、、、、、、、、14:08

---
+++ src/main/java/cn/java/model/Teacher.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:sessionId
+ * File Name:Person.java
+ * Package Name:cn.java.model
+ * Date:2017年10月14日下午1:46:06
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ *
+ */
+
+package cn.java.model;
+
+/**
+ * Description: <br/>
+ * Date: 2017年10月14日 下午1:46:06 <br/>
+ * 
+ * @author guoD
+ * @version
+ * @see
+ */
+public class Teacher {
+    public void show() {
+        System.out.println("我是修改后的教师。。。。。。。。。");
+    }
+}
