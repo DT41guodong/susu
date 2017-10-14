5ce4e527302c53837db3163656c796bc388e378c
DT41guodong <13627265857@163.com>
2017-10-14 13:54:43 +0800

    update

---
+++ src/main/java/cn/java/model/Person.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:sessionId
+ * File Name:Person.java
+ * Package Name:cn.java.model
+ * Date:2017年10月14日下午1:46:06
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ *
+ */
+
+package cn.java.model;
+
+/**
+ * Description: <br/>
+ * Date: 2017年10月14日 下午1:46:06 <br/>
+ * 
+ * @author guoD
+ * @version
+ * @see
+ */
+public class Person {
+    public void show() {
+        System.out.println("1");
+    }
+}
