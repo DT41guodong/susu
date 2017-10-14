8751ae06141ae3cb935fd264a3da7751f08903cc
DT41guodong <13627265857@163.com>
2017-10-14 13:44:04 +0800

    tttt

---
+++ src/main/java/cn/java/model/Student.java
@@ -0,0 +1,21 @@
+/**
+ * Project Name:sessionId
+ * File Name:Student.java
+ * Package Name:cn.java.model
+ * Date:2017年10月14日下午1:31:57
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ *
+*/
+
+package cn.java.model;
+/**
+ * Description:	   <br/>
+ * Date:     2017年10月14日 下午1:31:57 <br/>
+ * @author   guoD
+ * @version  
+ * @see 	 
+ */
+public class Student {
+
+}
+
